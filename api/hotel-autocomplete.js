80b188c70574102e48b08079430149b53e93098c
ehackshaw <hackshaw72@gmail.com>
2026-07-15 12:04:07 -0400

    Update hotel-autocomplete.js

--- api/hotel-autocomplete.js
+++ api/hotel-autocomplete.js
@@ -7,10 +7,32 @@ export default async function handler(req, res) {
   );
 
 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS"
+  );
+
+
+  if(req.method === "OPTIONS"){
+    return res.status(200).end();
+  }
+
+
+
   try {
 
 
     const query = req.query.q;
+
+
+    if(!query){
+
+      return res.status(200).json({
+        suggestions:[]
+      });
+
+    }
+
 
 
     const apiKey = process.env.SERPAPI_KEY;
@@ -27,19 +49,18 @@ export default async function handler(req, res) {
 
       gl:"us",
 
+      ll:"@25.7617,-80.1918,10z",
+
       api_key:apiKey
 
     });
 
 
 
-    const url =
-    "https://serpapi.com/search.json?" +
-    params.toString();
-
-
-
-    const response = await fetch(url);
+    const response = await fetch(
+      "https://serpapi.com/search.json?" +
+      params.toString()
+    );
 
 
 
@@ -53,11 +74,67 @@ export default async function handler(req, res) {
 
 
 
-    return res.status(200).json(data);
+    let suggestions=[];
+
+
+
+    if(Array.isArray(data.suggestions)){
+
+
+      suggestions =
+      data.suggestions.map(item=>{
+
+
+        const place =
+        item.data || item;
+
+
+
+        return {
+
+          name:
+          place.title ||
+          place.name ||
+          "",
+
+
+          description:
+          place.subtitle ||
+          place.description ||
+          "",
+
+
+          id:
+          place.place_id ||
+          "",
+
+
+          type:
+          place.type ||
+          "place"
+
+        };
+
+
+      });
+
+
+    }
+
+
+
+    return res.status(200).json({
+
+      suggestions
+
+    });
 
 
 
   } catch(error){
+
+
+    console.error(error);
 
 
     return res.status(500).json({
